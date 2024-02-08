af3659c3095e89661fb3f6fb614b86051fe30bb9
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2024-02-07 22:33:55 -0300

    Disable tests

--- src/test/java/com/pedro/rest/RestApplicationTests.java
+++ src/test/java/com/pedro/rest/RestApplicationTests.java
@@ -1,11 +1,13 @@
 package com.pedro.rest;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@Disabled
 class RestApplicationTests {
 
     @Test
--- src/test/java/com/pedro/rest/controller/EmployeeControllerTest.java
+++ src/test/java/com/pedro/rest/controller/EmployeeControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pedro.rest.model.Employee;
 import com.pedro.rest.service.EmployeeService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Disabled
 class EmployeeControllerTest {
 
     @Autowired
