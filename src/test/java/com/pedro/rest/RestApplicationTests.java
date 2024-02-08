a110b3916ae6da64f47585fe206c611261fccf1c
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2024-02-07 23:42:16 -0300

    Update Integration Tests

--- src/test/java/com/pedro/rest/RestApplicationTests.java
+++ src/test/java/com/pedro/rest/RestApplicationTests.java
@@ -1,13 +1,13 @@
 package com.pedro.rest;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@Disabled
+@ActiveProfiles("test")
 class RestApplicationTests {
 
     @Test
--- src/test/java/com/pedro/rest/controller/EmployeeControllerTest.java
+++ src/test/java/com/pedro/rest/controller/EmployeeControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -23,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Disabled
+@ActiveProfiles("test")
 class EmployeeControllerTest {
 
     @Autowired
